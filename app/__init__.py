473e534d37a22313cb3869710865b479b3704fcd
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2025-01-18 11:15:01 +0530

    CORS Added for frontend api call

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,12 @@ from .utils.dbutil import DBUtil
 from .utils.routeutil import RouteUtil
 from .routes import index
 from .initial_db_data import InitialDBData
+from flask_cors import CORS
+
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     dbutil = DBUtil()
     with app.app_context():
