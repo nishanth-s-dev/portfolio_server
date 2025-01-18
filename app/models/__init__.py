9eac43f46e09e3a9c62872a66a1b9a3f492cc146
Nishanth S <nishanth.selvakumar.dev@gmail.com>
2025-01-18 08:06:45 +0530

    change single imports to all

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,7 +2,4 @@ from app.utils.dbutil import DBUtil
 
 db = DBUtil().db
 
-from .player import Player
-from .project import Project
-from .connection_type import ConnectionType
-from .connection import Connection
+from . import *
